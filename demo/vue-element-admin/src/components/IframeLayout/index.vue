6ac8fbc64e94e6b20ec0861e9841207d0445b084
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-07-04 12:28:26 +0200

    add iframe with golanglibs.com for demo

---
+++ demo/vue-element-admin/src/components/IframeLayout/index.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div id="preview">
+        <div>
+            <div>Global: {{global.content}}</div>
+            <textarea v-model="global.content"></textarea>
+        </div>
+
+        <div>
+            <div>Local: {{local.content}}</div>
+            <textarea v-model="local.content"></textarea>
+        </div>
+    </div>
+
+</template>
+
+<script>
+// import Vue from 'vue'
+export default {
+  data() {
+    return this.$root.$data
+  }
+}
+</script>
+
+<style scoped>
+#preview {
+	left: 0px;
+	top: 0px;
+	border: 0px;
+	position: absolute;
+}
+</style>
